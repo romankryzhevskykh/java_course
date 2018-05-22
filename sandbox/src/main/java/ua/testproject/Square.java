af59ff952fc04b6fdeba5aba8f274099f630cf75
romakryzhevskykh <roman.kryzhevskykh@zaelab.com>
2018-05-22 22:31:18 +0300

    some classes were added

---
+++ sandbox/src/main/java/ua/testproject/Square.java
@@ -0,0 +1,13 @@
+package ua.testproject;
+
+public class Square {
+    public double l;
+
+    public Square(double l) {
+        this.l = l;
+    }
+
+    public double area() {
+        return this.l * this.l;
+    }
+}
--- sandbox/src/main/java/ua/testproject/Test.java
+++ sandbox/src/main/java/ua/testproject/Test.java
@@ -2,6 +2,21 @@ package ua.testproject;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        hello("world");
+        hello("user");
+
+        Square s = new Square(5);
+        System.out.println(s.l + " = " + s.area());
+
+        Rectangle r = new Rectangle(4, 6);
+        System.out.println("треугольник " + r.a + " " + r.b + " = " + r.area());
+
     }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+
+
 }
