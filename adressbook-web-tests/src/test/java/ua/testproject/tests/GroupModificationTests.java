f2a9ec04bea0d14a6486430cddd1e1335e2bcedd
romakryzhevskykh <roman.kryzhevskykh@zaelab.com>
2018-06-03 20:21:13 +0300

    modification test was added

---
+++ adressbook-web-tests/src/test/java/ua/testproject/tests/GroupModificationTests.java
@@ -0,0 +1,19 @@
+package ua.testproject.tests;
+
+import org.testng.annotations.Test;
+import ua.testproject.model.GroupData;
+
+
+public class GroupModificationTests extends TestBase {
+
+    @Test
+    public void testGroupModification() {
+        app.getNavigationHelper().goToGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
+    }
+
+}
