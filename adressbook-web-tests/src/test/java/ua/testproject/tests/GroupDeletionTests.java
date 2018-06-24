98e74917d85c081f2bdd7164752832c924df6ea9
romakryzhevskykh <roman.kryzhevskykh@zaelab.com>
2018-06-24 14:19:57 +0300

    precondition for groups tests were added

--- adressbook-web-tests/src/test/java/ua/testproject/tests/GroupDeletionTests.java
+++ adressbook-web-tests/src/test/java/ua/testproject/tests/GroupDeletionTests.java
@@ -9,7 +9,7 @@ public class GroupDeletionTests extends TestBase {
     public void testGroupDeletion() {
         app.getNavigationHelper().goToGroupPage();
         if(! app.getGroupHelper().isThereGroup()) {
-            app.getGroupHelper().createGroup(new GroupData("test1",null,null));
+            app.getGroupHelper().createGroup(new GroupData("test1","test2","test3"));
         }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSelectedGroups();
--- adressbook-web-tests/src/test/java/ua/testproject/tests/GroupModificationTests.java
+++ adressbook-web-tests/src/test/java/ua/testproject/tests/GroupModificationTests.java
@@ -9,6 +9,9 @@ public class GroupModificationTests extends TestBase {
     @Test
     public void testGroupModification() {
         app.getNavigationHelper().goToGroupPage();
+        if(! app.getGroupHelper().isThereGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("test1","test2","test3"));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
