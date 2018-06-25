480c1ad3de7e9f2048e7324b3dd3cf5563af2e12
romakryzhevskykh <roman.kryzhevskykh@zaelab.com>
2018-06-25 21:27:50 +0300

    preconditions lists were added

--- adressbook-web-tests/src/test/java/ua/testproject/tests/GroupCreationTests.java
+++ adressbook-web-tests/src/test/java/ua/testproject/tests/GroupCreationTests.java
@@ -4,15 +4,17 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ua.testproject.model.GroupData;
 
+import java.util.List;
+
 public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreationTests() {
         app.getNavigationHelper().goToGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
+        List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before + 1);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size() + 1);
     }
 
 }
--- adressbook-web-tests/src/test/java/ua/testproject/tests/GroupModificationTests.java
+++ adressbook-web-tests/src/test/java/ua/testproject/tests/GroupModificationTests.java
@@ -4,23 +4,25 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ua.testproject.model.GroupData;
 
+import java.util.List;
+
 
 public class GroupModificationTests extends TestBase {
 
     @Test
     public void testGroupModification() {
         app.getNavigationHelper().goToGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
         if(! app.getGroupHelper().isThereGroup()) {
             app.getGroupHelper().createGroup(new GroupData("test1","test2","test3"));
         }
-        app.getGroupHelper().selectGroup();
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size());
     }
 
 }
