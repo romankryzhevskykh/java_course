919059cf06e1fb5213637e2d9beb4e44c6200212
romakryzhevskykh <roman.kryzhevskykh@zaelab.com>
2018-07-02 22:48:58 +0300

    hash set assertions were added

--- adressbook-web-tests/src/test/java/ua/testproject/model/GroupData.java
+++ adressbook-web-tests/src/test/java/ua/testproject/model/GroupData.java
@@ -1,11 +1,24 @@
 package ua.testproject.model;
 
 public class GroupData {
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private int id;
     private final String name;
     private final String header;
     private final String footer;
 
+    public GroupData(int id, String name, String header, String footer) {
+        this.id = id;
+        this.name = name;
+        this.header = header;
+        this.footer = footer;
+    }
+
     public GroupData(String name, String header, String footer) {
+        this.id = 0;
         this.name = name;
         this.header = header;
         this.footer = footer;
@@ -19,7 +32,40 @@ public class GroupData {
         return header;
     }
 
+
+
     public String getFooter() {
         return footer;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GroupData groupData = (GroupData) o;
+
+        if (id != groupData.id) return false;
+        return name != null ? name.equals(groupData.name) : groupData.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
 }
--- adressbook-web-tests/src/test/java/ua/testproject/tests/GroupDeletionTests.java
+++ adressbook-web-tests/src/test/java/ua/testproject/tests/GroupDeletionTests.java
@@ -11,8 +11,8 @@ public class GroupDeletionTests extends TestBase {
     @Test
     public void testGroupDeletion() {
         app.getNavigationHelper().goToGroupPage();
-        if(! app.getGroupHelper().isThereGroup()) {
-            app.getGroupHelper().createGroup(new GroupData("test1","test2","test3"));
+        if (!app.getGroupHelper().isThereGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
         }
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
@@ -20,6 +20,9 @@ public class GroupDeletionTests extends TestBase {
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() - 1);
+
+        before.remove(before.size() - 1);
+        Assert.assertEquals(before, after);
     }
 
 }
